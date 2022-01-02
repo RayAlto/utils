feb8cd8bc370e45db7bad899b54add1c4157d3e7
RayAlto <tianjiayu668@gmail.com>
2022-01-02 22:46:49 +0800

    🎨Format

--- include/strtool.hpp
+++ include/strtool.hpp
@@ -10,91 +10,66 @@ namespace rayalto {
 namespace utils {
 namespace string {
 
-/**
- * Trim out space characters from string (from start)
- */
+// Trim out space characters from string (from start)
 std::string lstrip(std::string& str);
 std::string lstrip(const std::string& str);
 
-/**
- * Trim out space characters from string (from end)
- */
+// Trim out space characters from string (from end)
 std::string rstrip(std::string& str);
 std::string rstrip(const std::string& str);
 
-/**
- * Trim out space characters from string (from both ends)
- */
+// Trim out space characters from string (from both ends)
 std::string strip(std::string& str);
 std::string strip(const std::string& str);
 
-/**
- * Trim out specified characters from string (from start)
- */
+// Trim out specified characters from string (from start)
 std::string lstrip(std::string& str, const std::unordered_set<char>& chars);
-std::string lstrip(const std::string& str, const std::unordered_set<char>& chars);
+std::string lstrip(const std::string& str,
+                   const std::unordered_set<char>& chars);
 
-/**
- * Trim out specified characters from string (from end)
- */
+// Trim out specified characters from string (from end)
 std::string rstrip(std::string& str, const std::unordered_set<char>& chars);
-std::string rstrip(const std::string& str, const std::unordered_set<char>& chars);
+std::string rstrip(const std::string& str,
+                   const std::unordered_set<char>& chars);
 
-/**
- * Trim out specified characters from string (from both ends)
- */
+// Trim out specified characters from string (from both ends)
 std::string strip(std::string& str, const std::unordered_set<char>& chars);
-std::string strip(const std::string& str, const std::unordered_set<char>& chars);
+std::string strip(const std::string& str,
+                  const std::unordered_set<char>& chars);
 
-/**
- * Split string with specified char
- */
+// Split string with specified char
 std::vector<std::string> split(const std::string& str, const char& sep);
 
-/**
- * Split string with specified string
- */
+// Split string with specified string
 std::vector<std::string> split(const std::string& str, const std::string& sep);
 
-/**
- * Split string with specified char and with callback to determine whether to leave a line
- */
+// Split string with specified char and with callback to determine whether to leave a line
 std::vector<std::string> split(
     const std::string& str,
     const char& sep,
     std::function<bool(std::string& line)>& callback);
 
-/**
- * Split string with specified string and with callback to determine whether to leave a line
- */
+// Split string with specified string and with callback to determine whether to leave a line
 std::vector<std::string> split(
     const std::string& str,
     const std::string& sep,
     std::function<bool(std::string& line)>& callback);
 
-/**
- * Split string with specified char and with callback to determine whether to leave a line
- */
+// Split string with specified char and with callback to determine whether to leave a line
 std::vector<std::string> split(const std::string& str,
                                const char& sep,
                                bool (*callback)(std::string& line));
 
-/**
- * Split string with specified string and with callback to determine whether to leave a line
- */
+// Split string with specified string and with callback to determine whether to leave a line
 std::vector<std::string> split(const std::string& str,
                                const std::string& sep,
                                bool (*callback)(std::string& line));
 
-/**
- * Split string with specified char only once
- */
+// Split string with specified char only once
 std::pair<std::string, std::string> split_once(const std::string& str,
                                                const char& sep);
 
-/**
- * Split string with specified string only once
- */
+// Split string with specified string only once
 std::pair<std::string, std::string> split_once(const std::string& str,
                                                const std::string& sep);
 
