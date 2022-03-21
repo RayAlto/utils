e6ab6f8bfc69e495a69d6e4d3012945c27679cef
RayAlto <tianjiayu668@gmail.com>
2022-03-21 12:03:20 +0800

    🔥 Remove: websocket, libwebsockets sucks!

--- test/test_websocket.cc
+++ test/test_websocket.cc
@@ -1,21 +1,6 @@
-#include <chrono>
 #include <iostream>
-#include <thread>
-
-#include <websocket/ws_client.h>
-
-using rayalto::utils::WsClient;
 
 int main(int argc, char const *argv[]) {
-    WsClient client;
-    client.remote_address("127.0.0.1");
-    client.remote_path("/foo");
-    client.remote_port(8080);
-    client.header_host("127.0.0.1");
-    client.header_origin("127.0.0.1");
-    auto foo = client.run();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-        client.send("hello");
-    std::this_thread::sleep_for(std::chrono::seconds(100));
+    std::cout << "hello world" << std::endl;
     return 0;
 }
