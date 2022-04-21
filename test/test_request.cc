859508ee533089b609df40e5a04d1c143ad3c4e2
RayAlto <tianjiayu668@gmail.com>
2022-04-21 10:36:15 +0800

    ✅ Request: update test program

--- test/test_request.cc
+++ test/test_request.cc
@@ -11,30 +11,36 @@ using namespace rayalto::utils::request;
 
 int main(int argc, char const *argv[]) {
     Request request;
-    request.url("https://httpbin.org/anything");
-    request.method(Method::POST);
-    request.useragent("RayAlto/114514");
-    request.header({{"foo", "bar"}, {"114514", "1919810"}});
-    request.cookie({{"brand", "Nabisco Oreo"}, {"comment", "delicious"}});
+    // clang-format off
+    request.url("https://httpbin.org/anything")
+           .method(Method::POST)
+           .useragent("RayAlto/114514")
+           .header({
+                {"foo",    "bar"    },
+                {"114514", "1919810"}
+           })
+           .cookie({
+                {"brand",   "Nabisco Oreo"},
+                {"comment", "delicious"   }
+           })
+           .mime_parts({
+                {/* part name */ "file",
+                 /* part data */ MimePart()
+                     .is_file(true)
+                     .data(/* local file name */"example.png")
+                     .file_name(/* remote file name */"senpai.png")
+                     .type(MimeTypes::get("png"))
+                },
+                {/* part name */ "data",
+                 /* part data */ MimePart()
+                     .data(R"+*({"kimochi": "1919810", "come": "114514"})+*")
+                     .type(MimeTypes::get("json"))
+                }
+           })
+           .request();
+    // clang-format on
     /* request.body(R"+*+*({"age": 114514, "role": "student"})+*+*", */
     /*              MimeTypes::get("json")); */
-    // clang-format off
-    request.mime_parts({
-        {/* part name */ "file",
-         /* part data */ MimePart()
-            .is_file(true)
-            .data("example.png")
-            .file_name("senpai.png")
-            .type(MimeTypes::get("png"))
-        },
-        {/* part name */ "data",
-         /* part data */ MimePart()
-            .data(R"+*({"kimochi": "1919810", "come": "114514"})+*")
-            .type(MimeTypes::get("json"))
-        }
-    });
-    // clang-format on
-    request.request();
     Response response = request.response();
     std::cout << response.body << std::endl;
     return 0;
