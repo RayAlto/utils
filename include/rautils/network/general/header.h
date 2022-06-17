9b72ac0b57802a5f014a11c7ed04c3315cc544b0
RayAlto <tianjiayu668@gmail.com>
2022-06-17 14:57:08 +0800

    ♻ make network::general::Header a alias since it does not need extra func

--- include/rautils/network/general/header.h
+++ include/rautils/network/general/header.h
@@ -1,10 +1,6 @@
 #ifndef RA_UTILS_RAUTILS_NETWORK_GENERAL_HEADER_H_
 #define RA_UTILS_RAUTILS_NETWORK_GENERAL_HEADER_H_
 
-#include <initializer_list>
-#include <string>
-#include <utility>
-
 #include "rautils/misc/map_handler.h"
 
 namespace rayalto {
@@ -12,20 +8,7 @@ namespace utils {
 namespace network {
 namespace general {
 
-class Header : public misc::DictIcHandler {
-public:
-    Header(
-        std::initializer_list<std::pair<const std::string, std::string>> pairs);
-    Header(const misc::DictIC& map);
-    Header(misc::DictIC&& map);
-    Header() = default;
-    Header(const Header& header);
-    Header(Header&& header) noexcept;
-    Header& operator=(const Header& header);
-    Header& operator=(Header&& header) noexcept;
-
-    virtual ~Header() = default;
-};
+using Header = misc::DictIcHandler;
 
 } // namespace general
 } // namespace network
