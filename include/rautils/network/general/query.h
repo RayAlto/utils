4210222f66c07a06bf6768818fa33fdf1c8a545f
RayAlto <tianjiayu668@gmail.com>
2022-06-17 14:58:50 +0800

    ✨ Add network::general::Query

---
+++ include/rautils/network/general/query.h
@@ -0,0 +1,18 @@
+#ifndef RA_UTILS_RAUTILS_NETWORK_GENERAL_QUERY_H_
+#define RA_UTILS_RAUTILS_NETWORK_GENERAL_QUERY_H_
+
+#include "rautils/misc/map_handler.h"
+
+namespace rayalto {
+namespace utils {
+namespace network {
+namespace general {
+
+using Query = misc::DictHandler;
+
+} // namespace general
+} // namespace network
+} // namespace utils
+} // namespace rayalto
+
+#endif // RA_UTILS_RAUTILS_NETWORK_GENERAL_QUERY_H_
