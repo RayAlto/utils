fdd64339a444d0038601f4941a61336ea126664c
RayAlto <tianjiayu668@gmail.com>
2022-06-20 14:40:55 +0800

    ✅ Update test for network::general::Url

---
+++ test/test_url.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "rautils/network/general/url.h"
+
+using rayalto::utils::network::general::Url;
+
+int main(int argc, char const *argv[]) {
+    Url url("https://www.google.com:443/search?q=url");
+    std::cout << url.to_string() << std::endl;
+    return 0;
+}
