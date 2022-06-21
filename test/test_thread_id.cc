bc893610da59d5c55613cbe6072d5625e246d993
RayAlto <tianjiayu668@gmail.com>
2022-06-21 21:47:42 +0800

    ✅ Update test for misc::thread_id

---
+++ test/test_thread_id.cc
@@ -0,0 +1,17 @@
+#include <chrono>
+#include <cstdio>
+#include <thread>
+
+#include "rautils/misc/thread_id.h"
+
+int main(int argc, char const *argv[]) {
+    for (int i = 0; i < 100; ++i) {
+        std::thread t([&]() -> void {
+            std::printf("%d\n", rayalto::utils::misc::thread_id());
+            std::this_thread::sleep_for(std::chrono::milliseconds(90));
+        });
+        t.detach();
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    return 0;
+}
