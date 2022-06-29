9d408b55ee70a7c2eeeeacd2ad25b1a3dff74d1b
RayAlto <tianjiayu668@gmail.com>
2022-06-29 17:30:37 +0800

    ✅ Update test for system::Subprocess

---
+++ test/test_subprocess.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "rautils/system/subprocess.h"
+
+using rayalto::utils::system::Subprocess;
+
+int main(int argc, char const *argv[]) {
+    Subprocess s;
+    s.args(Subprocess::Args {"echo", "hello world"}).run();
+    std::cout << "exit status: " << s.get_exit_status()
+              << "\nstdout: " << s.get_stdout()
+              << "\nstderr: " << s.get_stderr() << std::endl;
+    return 0;
+}
